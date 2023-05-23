27787c4f1ce012c705bdff6966a49ff1ca01a1dc
Magnus Glasder <mglasder@ethz.ch>
2023-05-23 21:54:10 +0200

    add loss func, remove recursion in forward

--- e2e/models/cnn1d.py
+++ e2e/models/cnn1d.py
@@ -1,40 +1,43 @@
+import torch.nn.functional as F
 from lightning import LightningModule
 from torch import nn
 from torch.optim import Adam
 
 
 class CNN1D(LightningModule):
-    def __init__(self):
+    def __init__(self, loss=F.mse_loss):
         super().__init__()
         self.save_hyperparameters()
 
+        self.loss = loss
+
         # TODO: finish model implementation
-        self.model = nn.Conv1d(in_channels=100, out_channels=100)
+        self.model = nn.Conv1d(in_channels=224, out_channels=224, kernel_size=24, stride=1, padding=0)
         # ...
 
     def forward(self, x):
-        x = self(x)
+        x = self.model(x)
         return x
 
     def training_step(self, batch, batch_idx):
         inputs, targets, _ = batch
         predictions = self(inputs)
-        train_loss = self.lossfunc(predictions, targets)
+        train_loss = self.loss(predictions, targets)
         return {"loss": train_loss, "preds": predictions, "targets": targets}
 
     def validation_step(self, batch, batch_idx):
         inputs, targets, _ = batch
         predictions = self(inputs)
-        val_loss = self.lossfunc(predictions, targets)
+        val_loss = self.loss(predictions, targets)
         return {"loss": val_loss, "preds": predictions, "targets": targets}
 
     def test_step(self, batch, batch_idx):
         return self.validation_step(batch, batch_idx)
 
     def predict_step(self, batch, batch_idx: int, dataloader_idx: int = 0):
-        imgs, targets, _ = batch
-        preds = self(imgs)
-        pred_loss = self.lossfunc(preds, targets)
+        inputs, targets, _ = batch
+        preds = self(inputs)
+        pred_loss = self.loss(preds, targets)
         return {"loss": pred_loss, "preds": preds, "targets": targets}
 
     def configure_optimizers(self):
