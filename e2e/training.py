04c0a23ffdc04d75032e87e4f7758970005f70c1
Magnus Glasder <mglasder@ethz.ch>
2023-05-23 21:52:07 +0200

    correct data dir

--- e2e/training.py
+++ e2e/training.py
@@ -1,14 +1,19 @@
+from pathlib import Path
+
 from lightning import Trainer
 
 from e2e.datamodules.height_seq import ShapeDataset, ShapePredictionDataModule
 from e2e.models.cnn1d import CNN1D
 
+DATA_DIR = Path("/Volumes/hornets/homes/mglasder/datasets/TrainingDev")
+
 
 def main():
     model = CNN1D()
-    datamodule = ShapePredictionDataModule(dataset=ShapeDataset())
 
-    trainer = Trainer()
+    datamodule = ShapePredictionDataModule(data_dir=DATA_DIR, dataset=ShapeDataset())
+
+    trainer = Trainer(max_epochs=10, logger=False)
     trainer.fit(model=model, datamodule=datamodule)
 
 
