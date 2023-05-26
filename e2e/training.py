66fb2a68fb0820872d4e388c144bc887d6b84819
Magnus Glasder <mglasder@ethz.ch>
2023-05-26 09:30:02 +0200

    no gpu, use wandb logger, local data dir

--- e2e/training.py
+++ e2e/training.py
@@ -1,19 +1,25 @@
 from pathlib import Path
 
 from lightning import Trainer
+from lightning.pytorch.loggers import WandbLogger
 
 from e2e.datamodules.height_seq import ShapeDataset, ShapePredictionDataModule
 from e2e.models.cnn1d import CNN1D
 
-DATA_DIR = Path("/Volumes/hornets/homes/mglasder/datasets/TrainingDev")
+NAS_DATA_DIR = Path("/Volumes/hornets/homes/mglasder/datasets/TrainingDev")
+MAC_DATA_DIR = Path("/Users/magnus/datasets/WAAM/TrainingDev")
+
+BATCH_SIZE = 16
 
 
 def main():
-    model = CNN1D()
+    model = CNN1D(batch_size=BATCH_SIZE)
+    logger = WandbLogger(project="waam-e2e-pre", log_model=False)
+    logger = False
 
-    datamodule = ShapePredictionDataModule(data_dir=DATA_DIR, dataset=ShapeDataset())
+    datamodule = ShapePredictionDataModule(batch_size=BATCH_SIZE, data_dir=MAC_DATA_DIR, dataset=ShapeDataset())
 
-    trainer = Trainer(max_epochs=10, logger=False)
+    trainer = Trainer(max_epochs=50, logger=logger, enable_checkpointing=False, gpu=False)
     trainer.fit(model=model, datamodule=datamodule)
 
 
