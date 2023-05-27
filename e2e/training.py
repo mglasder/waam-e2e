67d0030cf6481d6a952d714c3b57f9b07c7c3125
Magnus Glasder <mglasder@ethz.ch>
2023-05-27 09:37:23 +0200

    update

--- e2e/training.py
+++ e2e/training.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from lightning import Trainer
 from lightning.pytorch.loggers import WandbLogger
 
+from e2e.callbacks.plotting import PredictionPlotting
 from e2e.data.datamodule import ShapePredictionDataModule
 from e2e.data.dataset import ShapeDataset
 from e2e.models.cnn1d import CNN1D
@@ -15,12 +16,23 @@ BATCH_SIZE = 16
 
 def main():
     model = CNN1D(batch_size=BATCH_SIZE)
-    logger = WandbLogger(project="waam-e2e-pre", log_model=False)
-    logger = False
+    logger = WandbLogger(project="waam-e2e-pre", log_model="all", log_every_n_steps=10)
 
-    datamodule = ShapePredictionDataModule(batch_size=BATCH_SIZE, data_dir=MAC_DATA_DIR, dataset=ShapeDataset())
+    datamodule = ShapePredictionDataModule(
+        batch_size=BATCH_SIZE,
+        data_dir=MAC_DATA_DIR,
+        dataset=ShapeDataset(),
+    )
 
-    trainer = Trainer(max_epochs=50, logger=logger, enable_checkpointing=False, accelerator="cpu")
+    callbacks = [PredictionPlotting()]
+
+    trainer = Trainer(
+        max_epochs=20,
+        logger=logger,
+        enable_checkpointing=False,
+        accelerator="mps",
+        callbacks=callbacks,
+    )
     trainer.fit(model=model, datamodule=datamodule)
 
 
