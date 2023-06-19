a20c880fa994b4b0b01615807845f151f9fcba5d
Magnus Glasder <mglasder@ethz.ch>
2023-06-19 13:30:16 +0200

    simple 1d ccn with fc layer (mini version of 1d resnet, w/o residual unit)

--- e2e/models/cnn1d.py
+++ e2e/models/cnn1d.py
@@ -5,6 +5,19 @@ from torch import nn
 from torch.optim import Adam
 
 
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super().__init__()
+
+        self.bn = nn.BatchNorm1d(in_channels)
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, stride, padding)
+
+    def forward(self, x):
+        x = self.bn(x)
+        x = self.conv(x)
+        return x
+
+
 class CNN1D(LightningModule):
     def __init__(self, loss=F.mse_loss, seq_length=224, batch_size=16):
         super().__init__()
@@ -15,30 +28,34 @@ class CNN1D(LightningModule):
         self.loss = loss
         self.length_in = seq_length
         length_out = seq_length
-        k = 224  # kernel_size
-        d = 1  # dilation
-        s = 1  # stride
-        p = (k - 1) // 2  # padding, k is odd
+        k = 56  # kernel_size
+        s = 8  # stride
+        p = (k - s) // 2  # padding, k is odd
+        p = 0
+        out_sz1 = ((seq_length + 2 * p - k) // s) + 1
+        # out_sz2 = ((out_sz1 + 2 * p - k) // s) + 1
 
         # TODO: finish model implementation
         self.model = nn.Sequential(
+            # nn.ReLU(),
+            Block(in_channels=1, out_channels=1, kernel_size=k, stride=s, padding=p),
+            nn.ReLU(),
+            # Block(in_channels=16, out_channels=1, kernel_size=k, stride=s, padding=p),
+            # nn.ReLU(),
             nn.BatchNorm1d(num_features=1),
-            nn.Conv1d(in_channels=1, out_channels=224, kernel_size=k, stride=s, dilation=d, padding=0),
-            # nn.Conv1d(in_channels=1, out_channels=1, kernel_size=k, stride=s, dilation=d, padding=p),
-            # nn.Conv1d(in_channels=1, out_channels=224, kernel_size=k, stride=s, dilation=d, padding=p),
-            # nn.Conv1d(in_channels=224, out_channels=1, kernel_size=k, stride=s, dilation=d, padding=p),
-            # nn.Tanh(),
+            nn.Linear(in_features=out_sz1, out_features=224),
         )
         self.model.apply(self._init_weights)
 
     @staticmethod
     def _init_weights(m):
-        if type(m) == nn.Conv1d:
+        if type(m) == nn.Conv1d or type(m) == nn.Linear:
             torch.nn.init.xavier_uniform_(m.weight)
             m.bias.data.fill_(0.01)
 
     def forward(self, x):
-        x = self.model(x.view(-1, 1, 224))
+        x = x.view(-1, 1, 224)
+        x = self.model(x)
         return x.view(-1, 224)
 
     def training_step(self, batch, batch_idx):
@@ -65,5 +82,5 @@ class CNN1D(LightningModule):
         return {"loss": pred_loss, "preds": preds, "targets": targets}
 
     def configure_optimizers(self):
-        optimizer = Adam(self.model.parameters(), lr=0.005, weight_decay=0)
+        optimizer = Adam(self.model.parameters(), lr=0.01, weight_decay=0)
         return optimizer
