d08d5f04208bff0580f6e27a9c1fc72d3e77a691
Magnus Glasder <mglasder@ethz.ch>
2023-06-28 10:45:51 +0200

    dataset for just predicting next bead shape

--- e2e/data/dataset.py
+++ e2e/data/dataset.py
@@ -3,9 +3,10 @@ from typing import Optional, TypeVar
 
 import numpy as np
 import torch
+from scipy.signal import resample
 from torch.utils.data import Dataset
 
-from e2e.data.sample import CrossSectionSample
+from e2e.data.sample import CrossSectionSample, FootprintEdge
 
 IDs = TypeVar("IDs", bound=list[str])
 Samples = TypeVar("Samples", bound=list[CrossSectionSample])
@@ -102,3 +103,84 @@ class ShapeDataset(WaamDataset):
         right = torch_idx + self._seg_len // 2
         ys = np.array([p.y[0] for p in points])[left:right]
         return torch.tensor(ys, dtype=torch.float32)
+
+
+class ShapeV2Dataset(WaamDataset):
+    def __init__(self, output_length=100):
+        self.inputs: Optional[list[LineSegmentZ]] = None
+        self.targets: Optional[list[LineSegmentZ]] = None
+        self.footprint_idx: Optional[list[tuple[int, int]]] = None
+        self.ids: Optional[IDs] = None
+
+        self._out_len = output_length
+        self._seg_len = 224
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        inpt = self.inputs[idx]
+        target = self.targets[idx]
+        sample_id = self.ids[idx]
+        # footprint = self.footprint_idx[idx]
+
+        return inpt, target, sample_id
+
+    def create(self, samples: Samples) -> WaamDataset:
+        self.inputs = self._extract_inputs(samples)
+        self.targets = self._extract_targets(samples)
+        # self.footprint_idx = self._extract_footprint_idx(samples)
+        self.ids = self._get_ids(samples)
+        # TODO: mirror
+
+        return self
+
+    def _extract_inputs(self, samples: Samples) -> list[LineSegmentZ]:
+        inputs = []
+        for s in samples:
+            torch_idx = s.torchposition.global_y_idx
+            zs = self._get_segment_heights(s.slice_based_before.points, torch_idx)
+            inputs.append(zs)
+
+        return inputs
+
+    def _extract_targets(self, samples: Samples) -> list[LineSegmentZ]:
+        targets = []
+        for s in samples:
+            zs = self._get_output_segment_heights(s.slice_based_after.points, s.footprint_based)
+            targets.append(zs)
+
+        return targets
+
+    @staticmethod
+    def _get_ids(samples: Samples) -> list[str]:
+        ids = []
+        for s in samples:
+            id_ = s.experiment + "_" + str(s.bead_id)
+            ids.append(id_)
+        return ids
+
+    def _get_segment_heights(self, points, torch_idx) -> torch.Tensor:
+        """ys corresponds to global z-coordinates."""
+        left = torch_idx - self._seg_len // 2
+        right = torch_idx + self._seg_len // 2
+        ys = np.array([p.y[0] for p in points])[left:right]
+        return torch.tensor(ys, dtype=torch.float32)
+
+    def _get_output_segment_heights(self, points, footprint: FootprintEdge):
+        left = footprint.left_idx
+        right = footprint.right_idx
+
+        ys = np.array([p.y[0] for p in points])[left:right]
+        ys = resample(ys, num=self._out_len)
+        return torch.tensor(ys, dtype=torch.float32)
+
+    @staticmethod
+    def _extract_footprint_idx(samples: Samples) -> list[torch.tensor]:
+        footprint_idx = []
+        for s in samples:
+            footprint = s.footprint_based
+            left = footprint.left_idx
+            right = footprint.right_idx
+            footprint_idx.append(torch.tensor([left, right]))
+        return footprint_idx
