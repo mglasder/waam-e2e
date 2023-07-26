82cc61768c0024d9a4a07d5f41f73845857a1a9f
Magnus Glasder <mglasder@ethz.ch>
2023-07-26 10:37:00 +0200

    adjusted to provide footprint idx explicitly

--- e2e/callbacks/metrics.py
+++ e2e/callbacks/metrics.py
@@ -1,4 +1,3 @@
-# TODO: modified haussdorff distance per modified
 import numpy as np
 from lightning import Callback
 from scipy.spatial.distance import cdist
@@ -18,9 +17,10 @@ class FootprintAvgAbsValErrorLogger(Callback):
     ):
         preds = outputs["preds"]
         targets = outputs["targets"]
+        fp_idx = outputs["footprint"]
 
-        left_errors = preds[:, 0] - targets[:, 0]
-        right_errors = preds[:, -1] - targets[:, -1]
+        left_errors = preds[:, fp_idx[0]] - targets[:, fp_idx[1]]
+        right_errors = preds[:, fp_idx[0]] - targets[:, fp_idx[1]]
 
         trainer.logger.log_metrics({"val/footprint_error_left": left_errors.abs().mean()})
         trainer.logger.log_metrics({"val/footprint_error_right": right_errors.abs().mean()})
@@ -38,6 +38,7 @@ class ModHausdorffLogger(Callback):
     ):
         preds = outputs["preds"].detach().cpu().numpy()
         targets = outputs["targets"].detach().cpu().numpy()
+        fp_idx = outputs["footprint"].detach().cpu().numpy()
 
         # this is not really correct as the resolution is not 0.1 anymore
         x = np.arange(0, len(preds) / 10, 0.1)
@@ -45,7 +46,9 @@ class ModHausdorffLogger(Callback):
         # median of min distances
         modhaussdorff = []
         for b in range(outputs["preds"].shape[0]):
-            dist = cdist(list(zip(x, targets[b, :])), list(zip(x, preds[b, :])))
+            left = fp_idx[b, 0]
+            right = fp_idx[b, 1]
+            dist = cdist(list(zip(x, targets[b, left:right])), list(zip(x, preds[b, left:right])))
             modhaussdorff.append((np.median(np.min(dist, axis=1))))
 
         trainer.logger.log_metrics({"val/mod_haussdorff": float(np.mean(modhaussdorff))})
