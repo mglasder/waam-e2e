c04d70ca3fe4c9dbda6888f5cf40dd1adc5b877d
Magnus Glasder <mglasder@ethz.ch>
2023-07-26 18:35:41 +0200

    losses, smoothing layer as modules

--- e2e/models/modelV2.py
+++ e2e/models/modelV2.py
@@ -5,6 +5,109 @@ from torch import nn
 from torch.optim import Adam
 
 
+class SmoothnessLossMid(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, outputs, fp_idx):
+        # start_index, end_index = fp_idx[:, 0], fp_idx[:, 1]
+        # smoothness_loss = torch.mean(
+        #     (outputs[:, start_index : end_index - 1] - outputs[:, start_index + 1 : end_index]) ** 2
+        # )
+        # return smoothness_loss
+
+        batch_size = outputs.size(0)
+        max_len = outputs.size(1)
+
+        # Create a range of indices
+        idx = torch.arange(max_len).unsqueeze(0).repeat(batch_size, 1).to(outputs.device)
+
+        # Create masks for start and end indices
+        start_mask = idx >= fp_idx[:, 0:1]
+        end_mask = idx < fp_idx[:, 1:2]
+        mask = start_mask & end_mask
+
+        # Apply mask to get the relevant outputs
+        masked_outputs = outputs * mask.float()
+
+        # Calculate second derivative
+        second_derivative = masked_outputs[:, :-2] - 2 * masked_outputs[:, 1:-1] + masked_outputs[:, 2:]
+
+        # Apply mask to second derivative
+        second_derivative_masked = second_derivative * mask[:, :-2].float()
+
+        # Calculate smoothness loss
+        smoothness_loss = torch.mean(second_derivative_masked**2, dim=1)
+
+        return torch.mean(smoothness_loss)
+
+
+class SmoothnessLossPiecewise(nn.Module):
+    def __init__(self, n=5):
+        super().__init__()
+        self.n = n
+
+    def forward(self, outputs, fp_idx):
+        batch_size = outputs.size(0)
+        max_len = outputs.size(1)
+
+        # Create a range of indices
+        idx = torch.arange(max_len).unsqueeze(0).repeat(batch_size, 1).to(outputs.device)
+
+        # Create masks for start and end indices
+        left_mask = idx < fp_idx[:, 0:1]
+        middle_mask = (idx >= fp_idx[:, 0:1]) & (idx < fp_idx[:, 1:2])
+        right_mask = idx >= fp_idx[:, 1:2]
+
+        # Calculate second derivative
+        second_derivative = outputs[:, : -2 * self.n] - 2 * outputs[:, self.n : -self.n] + outputs[:, 2 * self.n :]
+
+        # Apply masks to second derivative
+        second_derivative_left = second_derivative * left_mask[:, : -2 * self.n].float()
+        second_derivative_middle = second_derivative * middle_mask[:, : -2 * self.n].float()
+        second_derivative_right = second_derivative * right_mask[:, : -2 * self.n].float()
+
+        # Calculate smoothness loss
+        smoothness_loss_left = torch.mean(second_derivative_left**2, dim=1)
+        smoothness_loss_middle = torch.mean(second_derivative_middle**2, dim=1)
+        smoothness_loss_right = torch.mean(second_derivative_right**2, dim=1)
+
+        # Combine the losses
+        smoothness_loss = smoothness_loss_left + smoothness_loss_middle + smoothness_loss_right
+
+        return torch.mean(smoothness_loss)
+
+
+class AreaLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.scaling_factor = 0.1
+
+    def forward(self, inputs, outputs, targets, fp_idx):
+        batch_size = outputs.size(0)
+        max_len = outputs.size(1)
+
+        # Create a range of indices
+        idx = torch.arange(max_len).unsqueeze(0).repeat(batch_size, 1).to(outputs.device)
+
+        # Create masks for start and end indices
+        start_mask = idx >= fp_idx[:, 0:1]
+        end_mask = idx < fp_idx[:, 1:2]
+        mask = start_mask & end_mask
+
+        # Apply mask to get the relevant outputs and targets
+        masked_outputs = outputs * mask.float()
+        masked_targets = targets * mask.float()
+
+        # Calculate areas
+        area_outputs = torch.sum(masked_outputs, dim=1)
+        area_targets = torch.sum(masked_targets, dim=1)
+
+        # Calculate absolute difference in areas
+        loss = torch.abs(area_outputs - area_targets)
+        return torch.mean(loss) * self.scaling_factor
+
+
 class ModelV2(LightningModule):
     def __init__(
         self,
@@ -34,6 +137,9 @@ class ModelV2(LightningModule):
 
         self.model = model
         self.model.apply(self._init_weights)
+
+        self._smoothness_loss = SmoothnessLossPiecewise(n=1)
+        self._area_loss = AreaLoss()
 
     @staticmethod
     def _init_weights(m):
@@ -93,26 +199,31 @@ class ModelV2(LightningModule):
         # smoothness_loss = self._smoothness(predictions.detach())
         # area_loss = self._area_loss(predictions, targets)
         # return (1.0 - self.lambda_ - self.gamma) * loss + self.lambda_ * smoothness_loss + self.gamma * area_loss
-        return (1 - self.theta - self.gamma) * loss + self.theta * footprint_loss  # + self.gamma * area_loss
+        return (
+            (1 - self.theta - self.gamma - self.lambda_) * loss
+            + self.theta * footprint_loss
+            + self.lambda_ * self._smoothness_loss(predictions, fp)
+            + self.gamma * self._area_loss(inputs, predictions, targets, fp)
+        )
 
     def _footprint_loss(self, preds, targets, fp):
         ldiff = preds[:, fp[0]] - targets[:, fp[1]]
         rdiff = preds[:, fp[0]] - targets[:, fp[1]]
         return torch.mean(ldiff**2 + rdiff**2)
 
-    @staticmethod
-    def _smoothness(preds):
-        dy = preds[:, 1:] - preds[:, :-1]
-        d2y = dy[:, 1:] - dy[:, :-1]
-        return torch.mean(torch.sum(d2y**2)) / 100
+    # @staticmethod
+    # def _smoothness(preds):
+    #     dy = preds[:, 1:] - preds[:, :-1]
+    #     d2y = dy[:, 1:] - dy[:, :-1]
+    #     return torch.mean(torch.sum(d2y**2)) / 100
 
-    @staticmethod
-    def _area_loss(inputs, targets, predictions):
-        # calculate are between inputs and predictions
-        diff_true = targets - inputs
-        x = torch.linspace(0, (119 * 0.1), 119)
-
-        diff_pred = predictions - inputs
-        area_true = torch.trapz(diff_true, x)
-        area_pred = torch.trapz(diff_pred, x)
-        return torch.mean(torch.abs(area_true - area_pred))
+    # @staticmethod
+    # def _area_loss(inputs, targets, predictions):
+    #     # calculate are between inputs and predictions
+    #     diff_true = targets - inputs
+    #     x = torch.linspace(0, (119 * 0.1), 119)
+    #
+    #     diff_pred = predictions - inputs
+    #     area_true = torch.trapz(diff_true, x)
+    #     area_pred = torch.trapz(diff_pred, x)
+    #     return torch.mean(torch.abs(area_true - area_pred))
