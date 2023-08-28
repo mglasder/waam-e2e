9ac80778c4ab86d0cad05f434e15e0e1d2bcf86f
Magnus Glasder <mglasder@ethz.ch>
2023-08-28 08:09:37 +0200

    training loop adapted

--- e2e/training.py
+++ e2e/training.py
@@ -1,16 +1,19 @@
 from pathlib import Path
 
+import numpy as np
 import torch
 from lightning import Trainer
+from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
+from torch.utils.data import DataLoader
 
 from e2e.callbacks.metrics import FootprintAvgAbsValErrorLogger, ModHausdorffLogger
 from e2e.callbacks.plotting import PredictionPlotting
 from e2e.data.datamodule import ShapePredictionDataModule
 from e2e.data.dataset import ShapeDataset
 from e2e.data.loader import EXPERIMENT as EXP
+from e2e.models.mlp import RNN
 from e2e.models.modelV2 import ModelV2
-from e2e.models.simpleconv import SimpleConv
 
 NAS_DATA_DIR_DEV = Path("/Volumes/hornets/homes/mglasder/datasets/TrainingDev")
 MAC_DATA_DIR_DEV = Path("/Users/magnus/datasets/WAAM/TrainingDev")
@@ -18,19 +21,19 @@ VM_DATA_DIR = Path("/home/magnus/datasets/waam/30_processing_results/ImageGenera
 VM_DATA_DIR_DEV = Path("/home/magnus/datasets/waam/TrainingDev")
 
 BATCH_SIZE = 16
-MAX_EPOCHS = 70
+MAX_EPOCHS = 50
 N_WORKERS = 16
 DEVICE = "cuda"
 # footprint
 THETA = 0.4
 # smoothness
-LAMBDA = 0.4
+LAMBDA = 0.3
 # area
 GAMMA = 0.05
 DEV_RUN = False
 LOGGING = True
 TARGET_LENGTH = 120
-LR = 0.0003
+LR = 0.0005
 P = 0.5
 
 if torch.cuda.is_available():
@@ -41,11 +44,17 @@ def main():
     # unet = UNet1D(enconf=EncoderConfig(), deconf=DecoderConfig())
     # unet.to(DEVICE)
 
-    conv = SimpleConv(in_channels=1, out_channels=TARGET_LENGTH, kernel_size=120, p=P)
-    conv.to(DEVICE)
+    # conv = SimpleConv(in_channels=1, out_channels=TARGET_LENGTH, kernel_size=120, p=P)
+    # conv.to(DEVICE)
+
+    # mlp = MLP(n_features=TARGET_LENGTH, p=P)
+    # mlp.to(DEVICE)
+
+    rnn = RNN(p=P, n_features=TARGET_LENGTH, n_hidden=TARGET_LENGTH, n_layers=10)
+    rnn.to(DEVICE)
 
     model = ModelV2(
-        model=conv,
+        model=rnn,
         batch_size=BATCH_SIZE,
         lr=LR,
         theta=THETA,
@@ -85,6 +94,15 @@ def main():
     if logger is not None:
         # callbacks.append(EarlyStopping(monitor="val_loss", patience=10, min_delta=0.001, mode="min"))
         # callbacks.append(PredictionPlotting(epochs=[]))
+        callbacks.append(
+            ModelCheckpoint(
+                every_n_epochs=10,
+                monitor="val_loss",
+                mode="min",
+                auto_insert_metric_name=True,
+                save_on_train_epoch_end=False,
+            )
+        )
         callbacks.append(PredictionPlotting(epochs=[]))
         callbacks.append(FootprintAvgAbsValErrorLogger())
         callbacks.append(ModHausdorffLogger())
@@ -92,12 +110,48 @@ def main():
     trainer = Trainer(
         max_epochs=MAX_EPOCHS,
         logger=logger,
-        enable_checkpointing=False,
+        enable_checkpointing=True,
         accelerator=DEVICE,
         callbacks=callbacks,
         log_every_n_steps=10,
     )
     trainer.fit(model=model, datamodule=datamodule)
+
+    val_data_loader = datamodule.val_dataloader()
+    mc_predictions(model, val_data_loader, "val", save=True)
+    train_data_loader = datamodule.train_dataloader()
+    mc_predictions(model, train_data_loader, "train", save=True)
+
+
+def mc_predictions(model, data_loader: DataLoader, stage: str, save=True):
+    mean_predictions = []
+    uncertainties = []
+    errors = []
+    xs = []
+    ys = []
+    ids = []
+
+    for i, batch in enumerate(data_loader):
+        x, y, id_, _ = batch
+        mean_prediction, uncertainty = model.predict_with_uncertainty(x, num_samples=150)
+        error = np.abs(mean_prediction.detach().cpu().numpy() - y.detach().cpu().numpy()).tolist()
+
+        mean_predictions.append(mean_prediction.detach().cpu().numpy().tolist())
+        uncertainties.append(uncertainty.detach().cpu().numpy().tolist())
+        errors.append(error)
+        xs.append(x.detach().cpu())
+        ys.append(y.detach().cpu())
+        ids.append(id_)
+
+        if save:
+            # save results to file
+            prefix = Path("outputs/mc_preds")
+            torch.save(mean_predictions, prefix / f"mean_prediction_{stage}.pt")
+            torch.save(uncertainties, prefix / f"std_prediction_{stage}.pt")
+            torch.save(errors, prefix / f"true_errors_prediction_{stage}.pt")
+            torch.save(xs, prefix / f"x_{stage}.pt")
+            torch.save(ys, prefix / f"y_{stage}.pt")
+            torch.save(ids, prefix / f"ids_{stage}.pt")
 
 
 if __name__ == "__main__":
