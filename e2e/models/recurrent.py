67d868b0d267bc697b800325ba61b462393a657d
Magnus Glasder <mglasder@ethz.ch>
2023-08-29 12:09:06 +0200

    autocommit: giddy-feather-442

--- e2e/models/recurrent.py
+++ e2e/models/recurrent.py
@@ -39,9 +39,9 @@ class RNN(nn.Module):
         self.bn1(x)
         x = self.fc(x)
         x = F.relu(x)
-        x = F.dropout(x, p=self.p, training=self.training) + r0
+        x = F.dropout(x + r0, p=self.p, training=self.training)
 
-        # r1 = x
+        r1 = x
 
         h0 = torch.zeros(self.n_layers, x.size(0), self.n_hidden).requires_grad_().to(x.device)
         x = self.bn2(x)
@@ -54,4 +54,4 @@ class RNN(nn.Module):
         # x = self.bn2(x)
         # x = self.fc_out(x)
 
-        return x  # + r1
+        return x + r1
