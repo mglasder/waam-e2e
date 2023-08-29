8cba3975391e88a83bb0235b59311a1627565b84
Magnus Glasder <mglasder@ethz.ch>
2023-08-29 14:59:32 +0200

    autocommit: leafy-terrain-451

--- e2e/training.py
+++ e2e/training.py
@@ -29,11 +29,11 @@ MAX_EPOCHS = 50
 N_WORKERS = 16
 DEVICE = "cuda"
 # footprint
-THETA = 0.2
+THETA = 0.0
 # smoothness
-LAMBDA = 0.4
+LAMBDA = 0.0
 # area
-GAMMA = 0.05
+GAMMA = 0.0
 INPUT_LENGTH = 90
 TARGET_LENGTH = 90
 LR = 0.001
