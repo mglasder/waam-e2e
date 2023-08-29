c1f59e9890e3e4eaa71c7a6d9c2d090ea2587658
Magnus Glasder <mglasder@ethz.ch>
2023-08-29 10:42:50 +0200

    autocommit: test_run_id

--- e2e/training.py
+++ e2e/training.py
@@ -90,7 +90,6 @@ def main():
         # check whether remote or local machine
         if Path("/home/magnus").exists():
             # TODO: implement
-            print("small change")
             pass
         else:
             commit_hash = git_add_commit_with(message=f"{run_name}")
