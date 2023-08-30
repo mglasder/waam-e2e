06e9d85008def90c3f5a8e4672c868d371e16703
Magnus Glasder <mglasder@ethz.ch>
2023-08-30 13:59:35 +0200

    autocommit: copper-dream-516

--- e2e/models/layers.py
+++ e2e/models/layers.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+# import pytest
+
 
 class SmoothingLayer(nn.Module):
     def __init__(self, max_window_size):
@@ -13,8 +15,21 @@ class SmoothingLayer(nn.Module):
         # Ensure window_size is within valid range
         window_size = torch.clamp(self.window_size, 1, self.max_window_size)
         window_size = round(window_size.item())
+        window_size = window_size if window_size % 2 == 1 else window_size - 1
 
-        # Apply moving average
         padding = (window_size - 1) // 2
         smoothed = F.avg_pool1d(x, window_size, stride=1, padding=padding)
+
         return smoothed
+
+
+def test_smoothing_layer():
+    x = torch.randn(3, 1, 90)
+    print(x.shape)
+    smooth = SmoothingLayer(max_window_size=30)
+    print(smooth(x).shape)
+    assert smooth(x).shape == x.shape
+
+
+if __name__ == "__main__":
+    test_smoothing_layer()
