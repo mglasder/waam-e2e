66681941e208a2a0cb6961870dc8bd548982ec8f
Magnus Glasder <mglasder@ethz.ch>
2023-09-07 11:26:09 +0200

    refactoring v1 done

---
+++ e2e/prediction/end_to_end.py
@@ -0,0 +1,119 @@
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+from torch import nn
+
+from e2e.data.loader import SampleLoader
+from e2e.helpers.wandb import download_model
+from e2e.models.modelV2 import ModelV2
+
+# Assuming the necessary imports are present
+
+
+class DataLoader:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+    def load_samples(self, which):
+        loader = SampleLoader(self.data_dir)
+        samples = loader.load(which=which)
+        return sorted(samples, key=lambda x: x.bead_id)
+
+
+class ModelHandler:
+    def __init__(self, project_name, run_id, version, lstm: nn.Module):
+        self.model_path = download_model(project=project_name, run_id=run_id, version=version)
+        self.model = ModelV2.load_from_checkpoint(model=lstm, checkpoint_path=self.model_path)
+        self.model.eval()
+
+    def predict_with_uncertainty(self, input_data):
+        return self.model.predict_with_uncertainty(input_data.to(self.model.device), num_samples=150)
+
+
+class Plotter:
+    @staticmethod
+    def debug_input_plot(x, title: str):
+        fig, ax = plt.subplots(figsize=(10, 10))
+        x_ = np.arange(0, len(x)) / 10
+        ax.plot(x_, x)
+        ax.set_aspect("equal")
+        ax.set_title(f"{title}")
+        plt.show()
+
+    @staticmethod
+    def plot_e2e(predictions, labels, ground_truth):
+        fig, ax = plt.subplots(figsize=(15, 5), dpi=300)
+        for i in range(len(predictions)):
+            pred = predictions[i]
+            z_offset = np.mean(pred[130:150])
+            y = pred - z_offset
+            x = np.arange(0, len(y)) / 10
+            color = "black" if labels[i] == 1 else "blue"
+            ax.plot(x, y, color=color, alpha=1, linewidth=0.5)
+
+            y_true = ground_truth[i] - z_offset
+            ax.plot(x, y_true, color="green", alpha=0.5, linewidth=0.5)
+
+            ax.set_aspect("equal")
+        plt.show()
+
+
+class Predictor:
+    def __init__(self, output_length, model_handler):
+        self.model_handler = model_handler
+        self.output_length = output_length
+        self.hl = output_length // 2
+
+    def _get_next_input(self, cross_section_predicted, next_torch_idx):
+        next_input = cross_section_predicted[next_torch_idx - self.hl : next_torch_idx + self.hl]
+        h0 = next_input[self.hl]
+        next_input -= h0
+        return torch.tensor(next_input, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+
+    def _update_curr_cross_section_with_pred(self, curr_cross_section_sample, pred):
+        ys_predicted = curr_cross_section_sample.slice_based_before.ys
+        curr_torch_idx = curr_cross_section_sample.torchposition.global_y_idx
+        ys_predicted[curr_torch_idx - self.hl : curr_torch_idx + self.hl] = pred
+        return ys_predicted
+
+    def _handle_prediction(self, curr_input, curr_cross_section_sample, next_cross_section_sample=None):
+        pred, uncertainty = self.model_handler.predict_with_uncertainty(curr_input)
+        uncertainty_score = np.sum(uncertainty.detach().cpu().numpy())
+        pred = pred.detach().cpu().numpy()
+
+        cross_section_predicted = self._update_curr_cross_section_with_pred(curr_cross_section_sample, pred)
+
+        if uncertainty_score > 4.5:
+            cross_section_predicted = curr_cross_section_sample.slice_based_after.ys
+            label = 1
+        else:
+            label = 0
+
+        if next_cross_section_sample:
+            next_torch_idx = next_cross_section_sample.torchposition.global_y_idx
+            next_input = self._get_next_input(cross_section_predicted.copy(), next_torch_idx)
+            curr_input = next_input
+
+        return cross_section_predicted, label, curr_input
+
+    def predict(self, cross_section_samples, dataset):
+        curr_input = torch.tensor(dataset[0][0], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+        ground_truth = []
+        predictions = []
+        labels = []
+
+        LEN = len(dataset)
+
+        for i in range(LEN):
+            curr_cross_section_sample = cross_section_samples[i]
+            next_cross_section_sample = cross_section_samples[i + 1] if i != (LEN - 1) else None
+
+            cross_section_predicted, label, curr_input = self._handle_prediction(
+                curr_input, curr_cross_section_sample, next_cross_section_sample
+            )
+
+            ground_truth.append(curr_cross_section_sample.slice_based_before.ys)
+            predictions.append(cross_section_predicted)
+            labels.append(label)
+
+        return predictions, labels, ground_truth
--- e2e/testexp.py
+++ e2e/testexp.py
@@ -1,18 +1,12 @@
 from pathlib import Path
 
-import numpy as np
-import torch
-from matplotlib import pyplot as plt
-
 from e2e.data.dataset import ShapeDataset
-from e2e.data.loader import SampleLoader
 from e2e.helpers import timing
 
 timing.ENABLE_TIMING = True
 from e2e.data.loader import EXPERIMENT as EXP
-from e2e.helpers.wandb import download_model
-from e2e.models.modelV2 import ModelV2
 from e2e.models.recurrent import LSTM
+from e2e.prediction.end_to_end import DataLoader, ModelHandler, Plotter, Predictor
 
 PROJECT_NAME = "waam-e2e-pre"
 RUN_ID = "xtuq679p"
@@ -25,111 +19,19 @@ TARGET_LENGTH = 90
 P = 0.5
 
 
-HL = TARGET_LENGTH // 2
-
-
-def debug_input_plot(x, title: str):
-    fig, ax = plt.subplots(figsize=(10, 10))
-    x_ = np.arange(0, len(x)) / 10
-    ax.plot(x_, x)
-    ax.set_aspect("equal")
-    ax.set_title(f"{title}")
-    plt.show()
-
-
-def update_curr_cross_section_with_pred(curr_cross_section_sample, pred):
-    ys_predicted = curr_cross_section_sample.slice_based_before.ys
-    curr_torch_idx = curr_cross_section_sample.torchposition.global_y_idx
-    ys_predicted[curr_torch_idx - HL : curr_torch_idx + HL] = pred
-    return ys_predicted
-
-
-def get_next_input(cross_section_predicted, next_torch_idx):
-    next_input = cross_section_predicted[next_torch_idx - HL : next_torch_idx + HL]
-    h0 = next_input[HL]
-    next_input -= h0
-    return torch.tensor(next_input, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
-
-
-def plot_e2e(predictions, labels, ground_truth):
-    # TODO: plot e2e predictions and compare to ground truth
-    fig, ax = plt.subplots(figsize=(15, 5), dpi=300)
-    for i in range(len(predictions)):
-        pred = predictions[i]
-        z_offset = np.mean(pred[130:150])
-        y = pred - z_offset
-        x = np.arange(0, len(y)) / 10
-        color = "black" if labels[i] == 1 else "blue"
-        ax.plot(x, y, color=color, alpha=1, linewidth=0.5)
-
-        y_true = ground_truth[i] - z_offset
-        ax.plot(x, y_true, color="green", alpha=0.5, linewidth=0.5)
-
-        ax.set_aspect("equal")
-    plt.show()
-
-
 def main():
-    model_path = download_model(project=PROJECT_NAME, run_id=RUN_ID, version=VERSION)
     lstm = LSTM(
         p=P, n_input_features=INPUT_LENGTH, n_output_features=TARGET_LENGTH, n_hidden=TARGET_LENGTH * 3, n_layers=10
     )
-    model = ModelV2.load_from_checkpoint(model=lstm, checkpoint_path=model_path)
-    model.eval()
-
-    loader = SampleLoader(VM_DATA_DIR)
-    cross_section_samples = loader.load(which=[EXP.RANDOM_EX6])
-    cross_section_samples = sorted(cross_section_samples, key=lambda x: x.bead_id)
+    data_loader = DataLoader(VM_DATA_DIR)
+    cross_section_samples = data_loader.load_samples([EXP.RANDOM_EX6])
     dataset = ShapeDataset(mirror=False, segment_length=TARGET_LENGTH).create(cross_section_samples)
 
-    curr_input = torch.tensor(dataset[0][0], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
-    ground_truth = []
-    predictions = []
-    labels = []
+    model_handler = ModelHandler(PROJECT_NAME, RUN_ID, VERSION, lstm=lstm)
+    predictor = Predictor(TARGET_LENGTH, model_handler)
 
-    LEN = len(dataset)
-
-    for i in range(LEN):
-        curr_cross_section_sample = cross_section_samples[i]
-
-        if i != (LEN - 1):
-            next_cross_section_sample = cross_section_samples[i + 1]
-
-        id_ = curr_cross_section_sample.bead_id
-        print(f"Pred on weld bead id: {id_} of {LEN} (i={i})")
-
-        ground_truth.append(curr_cross_section_sample.slice_based_before.ys)
-
-        # debug_input_plot(curr_input.detach().cpu().numpy())
-
-        pred, uncertainty = model.predict_with_uncertainty(curr_input.to(model.device), num_samples=150)
-        uncertainty_score = np.sum(uncertainty.detach().cpu().numpy())
-        print(f"Uncertainty: {uncertainty_score}")
-        pred = pred.detach().cpu().numpy()
-
-        cross_section_predicted = update_curr_cross_section_with_pred(curr_cross_section_sample, pred)
-
-        # HACK: if first bead in row, use actual input
-        # is_first_in_row = (curr_cross_section_sample.welding_params["weld_bead_nr"] == 1) and (i > 0)
-        if uncertainty_score > 4.5:
-            cross_section_predicted = curr_cross_section_sample.slice_based_after.ys
-            labels.append(1)
-        else:
-            labels.append(0)
-
-        predictions.append(cross_section_predicted)
-
-        if i != (LEN - 1):
-            next_torch_idx = next_cross_section_sample.torchposition.global_y_idx
-            next_input = get_next_input(cross_section_predicted.copy(), next_torch_idx)
-            curr_input = next_input
-
-    plot_e2e(predictions, labels, ground_truth)
-
-    # for i, pred in enumerate(predictions):
-    #     plt.plot(pred)
-    #     plt.title(f"Predictions, weld bead nr={i+1}")
-    #     plt.show()
+    predictions, labels, ground_truth = predictor.predict(cross_section_samples, dataset)
+    Plotter.plot_e2e(predictions, labels, ground_truth)
 
 
 if __name__ == "__main__":
