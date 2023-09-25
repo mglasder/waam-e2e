3bc45c332261c579050e6e3ffd50c2438aed1f55
Magnus Glasder <mglasder@ethz.ch>
2023-09-25 11:49:11 +0200

    revert to helpful-pine-592

--- e2e/training.py
+++ e2e/training.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import pandas as pd
 import requests
 import torch
 from lightning import Trainer
@@ -25,7 +24,7 @@ VM_DATA_DIR_DEV = Path("/home/magnus/datasets/waam/TrainingDev")
 
 SEED = 2345078
 BATCH_SIZE = 16
-MAX_EPOCHS = 200
+MAX_EPOCHS = 100
 N_WORKERS = 16
 DEVICE = "cuda"
 # footprint
