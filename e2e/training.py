7e8e3c783177c2a747ae0be0515147969d013ea8
Magnus Glasder <mglasder@ethz.ch>
2023-09-28 11:54:04 +0200

    autocommit: solar-plant-646: no smoothness loss, rel. high energy and footprint loss

--- e2e/training.py
+++ e2e/training.py
@@ -108,7 +108,7 @@ def main(note: str = ""):
 
     datamodule = ShapePredictionDataModule(
         batch_size=BATCH_SIZE,
-        data_dir=MAC_DATA_DIR_DEV,
+        data_dir=VM_DATA_DIR,
         workers=N_WORKERS,
         dataset=ShapeDataset(mirror=MIRROR, segment_length=TARGET_LENGTH),
         split=split,
