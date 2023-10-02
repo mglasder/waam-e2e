9ce67ac4a97c252084d95e4d65398df221fc202f
Magnus Glasder <mglasder@ethz.ch>
2023-10-02 14:38:07 +0200

    autocommit: stoic-field-674: untrained smoothing, sigma=4.0

--- e2e/models/recurrent.py
+++ e2e/models/recurrent.py
@@ -43,7 +43,7 @@ class LSTM(nn.Module):
         #     sigma_inits=[0.3, 1.2, 3.0],
         # )
         #
-        self.postsmooth = GaussianSmoothing(window_size=15, sigma_init=3.0).requires_grad_(False)
+        self.postsmooth = GaussianSmoothing(window_size=15, sigma_init=4.0).requires_grad_(False)
 
     def forward(self, x):
         r0 = x
