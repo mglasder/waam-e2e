9b155bf6d129b0009633c29ba6b35868c1a15dec
Magnus Glasder <mglasder@ethz.ch>
2023-10-02 14:24:39 +0200

    autocommit: eager-deluge-670: bug fix in

--- e2e/mcpredict.py
+++ e2e/mcpredict.py
@@ -49,9 +49,7 @@ class McUncertainty:
         for i, batch in enumerate(data_loader):
             x, y, id_, fp = batch
 
-            mean_diff, uncertainty = self._model.predict_with_uncertainty(x, num_samples=150)
-
-            mean_prediction = x.detach() + mean_diff
+            mean_prediction, uncertainty = self._model.predict_with_uncertainty(x, num_samples=150)
 
             error = np.abs(mean_prediction.detach().cpu().numpy() - y.detach().cpu().numpy()).tolist()
             mean_predictions.append(mean_prediction.detach().cpu().numpy().tolist())
