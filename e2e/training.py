4ea2b0843371cd48cc19219ef0b897410c977c58
Magnus Glasder <mglasder@ethz.ch>
2023-10-02 13:48:03 +0200

    autocommit: easy-dew-664: test run

--- e2e/training.py
+++ e2e/training.py
@@ -24,13 +24,13 @@ MAC_DATA_DIR_DEV = Path("/Users/magnus/datasets/WAAM/TrainingDev")
 VM_DATA_DIR = Path("/home/magnus/datasets/waam/30_processing_results/ImageGenerator")
 VM_DATA_DIR_DEV = Path("/home/magnus/datasets/waam/TrainingDev")
 
-DATASET = MAC_DATA_DIR_DEV
+DATASET = VM_DATA_DIR
 
 SEED = 2345078
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 MAX_EPOCHS = 2
-N_WORKERS = 1
-DEVICE = "cpu"
+N_WORKERS = 16
+DEVICE = "cuda"
 # footprint
 THETA = 0.4
 # smoothness
@@ -160,7 +160,7 @@ def main(note: str = ""):
 if __name__ == "__main__":
     # get input from stdin
     if LOGGING:
-        note = "local debug"  # input("Enter run note: ")
+        note = input("Enter run note: ")
     else:
         note = ""
     main(note=note)
