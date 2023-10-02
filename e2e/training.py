9652b3173bc00719a702e4cb6bd081427a1a0e9f
Magnus Glasder <mglasder@ethz.ch>
2023-10-02 15:09:39 +0200

    autocommit: resilient-salad-678:

--- e2e/training.py
+++ e2e/training.py
@@ -29,8 +29,8 @@ DATASET = VM_DATA_DIR
 SEED = 2345078
 BATCH_SIZE = 32
 MAX_EPOCHS = 50
-N_WORKERS = 16
-DEVICE = "cuda"
+N_WORKERS = 1
+DEVICE = "cpu"
 # footprint
 THETA = 0.4
 # smoothness
@@ -159,7 +159,7 @@ def main(note: str = ""):
 if __name__ == "__main__":
     # get input from stdin
     if LOGGING:
-        note = input("Enter run note: ")
+        note = ""  # input("Enter run note: ")
     else:
         note = ""
     main(note=note)
