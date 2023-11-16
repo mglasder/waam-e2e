fdefc6a9d561c2ea94afcc9441290d8af05ff43f
Magnus Glasder <mglasder@ethz.ch>
2023-11-16 11:48:03 +0100

    autocommit: tough-wildflower-791: plotting all training data

--- e2e/training.py
+++ e2e/training.py
@@ -164,9 +164,9 @@ def main(note: str = ""):
     mc = McUncertainty(best_model, train_data_loader, val_data_loader, test_dataloader=test_data_loader, logger=logger)
     mc.predict()
     mc.calibrate(strategy="temperature_scaling")
-    mc.plot_predictions("train", log=LOGGING, take=10)
-    mc.plot_predictions("val", log=LOGGING, take=50)
-    mc.plot_predictions("test", log=LOGGING, take=None)
+    mc.plot_predictions("train", log=LOGGING, take=None)
+    mc.plot_predictions("val", log=LOGGING, take=20)
+    mc.plot_predictions("test", log=LOGGING, take=30)
     #
     # names = ["mean_predictions", "uncertainties_calib", "xs", "ys", "ids"]
     # # date and time up to seconds
