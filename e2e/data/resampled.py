f9eb98e7d485cfb3b67e272fa155a13bc87663f7
Magnus Glasder <mglasder@ethz.ch>
2023-12-08 09:45:31 +0100

    shape points dataset

--- e2e/data/resampled.py
+++ e2e/data/resampled.py
@@ -111,6 +111,110 @@ class ResampledShapeDataset(WaamDataset):
         return torch.tensor(ys_new, dtype=torch.float32)
 
 
+class ResampledShapePointsDataset(WaamDataset):
+    def __init__(self, mirror=False, segment_length=90):
+        self.inputs: Optional[list[LineSegmentZ]] = None
+        self.targets: Optional[list[LineSegmentZ]] = None
+        self.ids: Optional[IDs] = None
+        self.fp_idx: Optional[list[torch.tensor]] = None
+
+        self.mirror = mirror
+        self._seg_len = segment_length
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        inpt = self.inputs[idx]
+        target = self.targets[idx]
+        sample_id = self.ids[idx]
+        footprint = self.fp_idx[idx]
+
+        return inpt, target, sample_id, footprint
+
+    def create(self, samples: Samples) -> WaamDataset:
+        fp_indices = self._extract_footprint_idx(samples)
+        inputs = self._extract_inputs(samples, fp_indices)
+        targets = self._extract_targets(samples, fp_indices)
+        ids = self._get_ids(samples)
+
+        if self.mirror:
+            self.inputs, self.targets, self.ids, self.fp_idx = self._mirror_dataset(inputs, targets, ids, fp_indices)
+
+        else:
+            self.inputs, self.targets, self.ids, self.fp_idx = inputs, targets, ids, fp_indices
+
+        return self
+
+    def _extract_inputs(self, samples: Samples, fp_indices: list[torch.tensor]) -> list[LineSegmentZ]:
+        inputs = []
+        for s, fp_idx in zip(samples, fp_indices):
+            zs = self._get_resampled_segment_heights(s.slice_based_before.points, fp_idx)
+            inputs.append(zs)
+
+        return inputs
+
+    def _extract_targets(self, samples: Samples, fp_indices: list[torch.tensor]) -> list[LineSegmentZ]:
+        targets = []
+        for s, fp_idx in zip(samples, fp_indices):
+            zs = self._get_resampled_segment_heights(s.slice_based_after.points, fp_idx)
+            targets.append(zs)
+
+        return targets
+
+    @staticmethod
+    def _extract_footprint_idx(samples: Samples) -> list[torch.tensor]:
+        footprint_idx = []
+        for s in samples:
+            footprint = s.footprint_based
+            left = footprint.left_idx
+            right = footprint.right_idx
+            footprint_idx.append(torch.tensor([left, right]))
+        return footprint_idx
+
+    def _mirror_dataset(self, inputs, targets, sample_ids, fp_indices):
+        inputs_h = []
+        targets_h = []
+        sample_ids_h = []
+        footprint_idx_h = []
+
+        for inpt, target, id_, fp_idx in zip(inputs, targets, sample_ids, fp_indices):
+            inputs_h.append(inpt.flipud())
+            targets_h.append(target.flipud())
+            footprint_idx_h.append(fp_idx.flipud())
+            sample_ids_h.append(id_ + "_hflip")
+
+        inputs_h.extend(inputs)
+        targets_h.extend(targets)
+        sample_ids_h.extend(sample_ids)
+        footprint_idx_h.extend(fp_indices)
+
+        return inputs_h, targets_h, sample_ids_h, footprint_idx_h
+
+    @staticmethod
+    def _get_ids(samples: Samples) -> list[str]:
+        ids = []
+        for s in samples:
+            id_ = s.experiment + "_" + str(s.bead_id)
+            ids.append(id_)
+        return ids
+
+    def _get_resampled_segment_heights(self, points, fp_idx: torch.tensor) -> torch.Tensor:
+        """resamples the segment between footprint edges to be of length self._seg_len"""
+        left = fp_idx[0]
+        right = fp_idx[1]
+
+        zs = np.array([p.y[0] for p in points])[left:right]
+        xs = np.arange(0, len(zs)) / 10
+
+        # resample
+        f = interp1d(xs, zs, kind="linear")
+        xs_new = np.linspace(0, (len(zs) - 1) / 10, self._seg_len)
+        zs_new = f(xs_new)
+
+        return torch.stack([zs_new, xs_new], dtype=torch.float32)
+
+
 class ResampledE2EDataset(ResampledShapeDataset):
     def __init__(self, mirror=False, segment_length=90):
         super().__init__(mirror, segment_length)
