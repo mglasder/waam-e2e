57f95d7cc4b45f90a51b247395705df3364cea88
Magnus Glasder <mglasder@ethz.ch>
2023-12-08 17:04:35 +0100

    big refactor

--- e2e/callbacks/plotting.py
+++ e2e/callbacks/plotting.py
@@ -1,6 +1,8 @@
 import random
 from typing import Any, Optional
 
+import numpy as np
+import torch
 from lightning import Callback, LightningModule, Trainer
 from matplotlib import pyplot as plt
 
@@ -10,167 +12,63 @@ class PredictionPlotting(Callback):
         self.epochs = epochs
         self._subset_sz = subset_size
 
-    def on_train_batch_end(
-        self,
-        trainer: Trainer,
-        pl_module: LightningModule,
-        outputs: Any,
-        batch: Any,
-        batch_idx: int,
-    ):
-        if (trainer.current_epoch == trainer.max_epochs - 1) or (trainer.current_epoch in self.epochs):
-            images = []
-            captions = []
-            loss = outputs["loss"].cpu().detach().numpy()
+    def on_train_batch_end(self, trainer: Trainer, pl_module: LightningModule, outputs, batch, batch_idx: int):
+        final_epoch = trainer.max_epochs - 1
+        if self._should_log_predictions(trainer.current_epoch, final_epoch):
+            self._log_predictions(trainer, outputs)
 
-            selected_indices = self._select_indices(len(outputs["preds"]))
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
+        final_epoch = trainer.max_epochs - 1
+        if self._should_log_predictions(trainer.current_epoch, final_epoch):
+            self._log_predictions(trainer, outputs)
 
-            for index in selected_indices:
-                p, t, i, id_, fp = (
-                    outputs["preds"][index],
-                    outputs["targets"][index],
-                    outputs["inputs"][index],
-                    outputs["ids"][index],
-                    outputs["footprint"][index],
-                )
-                fig, caption = self._plot_example(p, t, i, id_, fp, loss, trainer.current_epoch, "train")
-                images.append(fig)
-                captions.append(caption)
-                plt.close()
+    def _should_log_predictions(self, current_epoch: int, final_epoch: int) -> bool:
+        return (current_epoch == final_epoch) or (current_epoch in self.epochs)
 
-            trainer.logger.log_image(
-                key="train/preds_plots",
-                images=images,
-                caption=captions,
+    def _log_predictions(self, trainer: Trainer, outputs: Any, stage="train"):
+        images, captions = [], []
+        loss = self._convert_to_numpy(outputs["loss"])
+
+        selected_indices = self._select_indices(len(outputs["preds"]), stage=stage)
+
+        for idx in selected_indices:
+            pred, trgt, inpt, id_ = (
+                outputs["preds"][idx],
+                outputs["targets"][idx],
+                outputs["inputs"][idx],
+                outputs["ids"][idx],
             )
+            figure, caption = self._plot_example(pred, trgt, inpt, id_, loss, trainer.current_epoch, stage=stage)
+            images.append(figure)
+            captions.append(caption)
+            plt.close()
 
-    def on_validation_batch_end(
-        self,
-        trainer,
-        pl_module,
-        outputs,
-        batch,
-        batch_idx,
-        dataloader_idx=0,
-    ):
-        if (trainer.current_epoch == trainer.max_epochs - 1) or (trainer.current_epoch in self.epochs):
-            images = []
-            captions = []
-            loss = outputs["loss"].cpu().detach().numpy()
-            for p, t, i, id_, fp in zip(
-                outputs["preds"],
-                outputs["targets"],
-                outputs["inputs"],
-                outputs["ids"],
-                outputs["footprint"],
-            ):
-                fig, caption = self._plot_example(p, t, i, id_, fp, loss, trainer.current_epoch, "val")
-                images.append(fig)
-                captions.append(caption)
-                plt.close()
+        trainer.logger.log_image(
+            key=f"{stage}/preds_plots",
+            images=images,
+            caption=captions,
+        )
 
-            trainer.logger.log_image(
-                key="val/preds_plots",
-                images=images,
-                caption=captions,
-            )
-
-    def _select_indices(self, batch_size: int) -> list[int]:
-        if self._subset_sz is None or self._subset_sz >= batch_size:
+    def _select_indices(self, batch_size: int, stage: str) -> list[int]:
+        if self._subset_sz is None or self._subset_sz >= batch_size or stage == "val":
             return list(range(batch_size))
         else:
             return random.sample(range(batch_size), self._subset_sz)
 
-    @staticmethod
-    def _plot_example(pred, trgt, inpt, id_, fp, batch_loss, epoch, stage):
+    def _plot_example(self, pred, trgt, inpt, id_, batch_loss, epoch, stage):
+        pred_np, trgt_np, inpt_np = map(self._convert_to_numpy, [pred, trgt, inpt])
+
         fig, ax = plt.subplots()
-        ax.plot(pred.cpu().detach().numpy(), color="red", label="pred")
-        ax.plot(trgt.cpu().numpy(), color="blue", label="target (after)")
-        ax.plot(inpt.cpu().detach().numpy(), color="black", label="input (before)")
-        ax.axvline(fp[0].cpu().detach().numpy(), color="green", label="fp left")
-        ax.axvline(fp[1].cpu().detach().numpy(), color="green", label="fp right")
+        ax.plot(trgt_np[1, :], trgt_np[0, :], color="blue", label="target (after)")
+        ax.plot(pred_np[1, :], pred_np[0, :], color="red", label="pred")
+        ax.plot(inpt_np[1, :], inpt_np[0, :], color="black", label="input (before)")
+
         caption = f"{stage} sample: {id_} - epoch: {epoch} \n batch_loss: {batch_loss:.4f}"
         ax.set_title(caption)
         ax.legend()
         return fig, caption
 
-
-class PredictionPlottingV2(Callback):
-    def __init__(self, epochs: list[int] = [], subset_size: Optional[int] = None):
-        self.epochs = epochs
-        self._subset_sz = subset_size
-
-    def on_train_batch_end(
-        self,
-        trainer: Trainer,
-        pl_module: LightningModule,
-        outputs: Any,
-        batch: Any,
-        batch_idx: int,
-    ):
-        if (trainer.current_epoch == trainer.max_epochs - 1) or (trainer.current_epoch in self.epochs):
-            images = []
-            captions = []
-            loss = outputs["loss"].cpu().detach().numpy()
-
-            selected_indices = self._select_indices(len(outputs["preds"]))
-
-            for index in selected_indices:
-                p, t, i, id_ = (
-                    outputs["preds"][index],
-                    outputs["targets"][index],
-                    outputs["inputs"][index],
-                    outputs["ids"][index],
-                )
-                fig, caption = self._plot_example(p, t, i, id_, loss, trainer.current_epoch, "train")
-                images.append(fig)
-                captions.append(caption)
-                plt.close()
-
-            trainer.logger.log_image(
-                key="train/preds_plots",
-                images=images,
-                caption=captions,
-            )
-
-    def on_validation_batch_end(
-        self,
-        trainer,
-        pl_module,
-        outputs,
-        batch,
-        batch_idx,
-        dataloader_idx=0,
-    ):
-        if (trainer.current_epoch == trainer.max_epochs - 1) or (trainer.current_epoch in self.epochs):
-            images = []
-            captions = []
-            loss = outputs["loss"].cpu().detach().numpy()
-            for p, t, i, id_ in zip(outputs["preds"], outputs["targets"], outputs["inputs"], outputs["ids"]):
-                fig, caption = self._plot_example(p, t, i, id_, loss, trainer.current_epoch, "val")
-                images.append(fig)
-                captions.append(caption)
-                plt.close()
-
-            trainer.logger.log_image(
-                key="val/preds_plots",
-                images=images,
-                caption=captions,
-            )
-
-    def _select_indices(self, batch_size: int) -> list[int]:
-        if self._subset_sz is None or self._subset_sz >= batch_size:
-            return list(range(batch_size))
-        else:
-            return random.sample(range(batch_size), self._subset_sz)
-
     @staticmethod
-    def _plot_example(pred, trgt, inpt, id_, batch_loss, epoch, stage):
-        fig, ax = plt.subplots()
-        ax.plot(pred.cpu().detach().numpy(), color="red", label="pred")
-        ax.plot(trgt.cpu().numpy(), color="blue", label="target (after)")
-        # ax.plot(inpt.cpu().detach().numpy(), color="black", label="input (before)")
-        caption = f"{stage} sample: {id_} - epoch: {epoch} \n batch_loss: {batch_loss:.4f}"
-        ax.set_title(caption)
-        ax.legend()
-        return fig, caption
+    def _convert_to_numpy(data: torch.tensor) -> np.ndarray:
+        """Move to cpu & convert to numpy."""
+        return data.cpu().detach().numpy() if data.requires_grad else data.cpu().numpy()
