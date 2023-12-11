0cf296992c471f1cf88ebb5cf33b4f2a1ac17511
Magnus Glasder <mglasder@ethz.ch>
2023-12-11 10:13:20 +0100

    autocommit: copper-lion-47: arch changed to incorporate residual better

--- e2e/models/recurrent.py
+++ e2e/models/recurrent.py
@@ -73,6 +73,7 @@ class ShapePointsModel(nn.Module):
 
         self.fc1 = nn.Linear(2 * n_input_features, 4 * n_output_features, bias=True)
         self.fc2 = nn.Linear(4 * n_input_features, 2 * n_output_features, bias=True)
+        self.fc_out = nn.Linear(2 * n_output_features, 2 * n_output_features, bias=True)
 
         self.apply(self._init_weights)
 
@@ -80,7 +81,7 @@ class ShapePointsModel(nn.Module):
         if isinstance(m, nn.Linear):
             nn.init.xavier_uniform_(m.weight)
             if m.bias is not None:
-                m.bias.data.fill_(3.0)
+                m.bias.data.fill_(1.5)
                 # with torch.no_grad():
                 #     m.bias.copy_(torch.linspace(0.1, 7.0, steps=m.bias.size(0)))
 
@@ -90,5 +91,7 @@ class ShapePointsModel(nn.Module):
         r = x
         x = self.fc1(x)
         x = F.tanh(F.dropout(x, p=self.p, training=self.training))  # + r
-        x = self.fc2(x) + r
+        x = self.fc2(x)
+        x = F.relu(F.dropout(x, p=self.p, training=self.training))
+        x = self.fc_out(x + r)
         return x.reshape(batch_sz, 2, -1)
